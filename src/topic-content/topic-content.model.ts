0e39820b30475811f9f19c552563b48a42765f8d
Prathamesh Ghorpade <123160215+PRATHAMESHG1234@users.noreply.github.com>
2024-04-10 13:53:24 +0530

    minor changes in models

--- src/topic-content/topic-content.model.ts
+++ src/topic-content/topic-content.model.ts
@@ -1,14 +1,14 @@
 import * as mongoose from 'mongoose';
 
 export const TopicContentSchema = new mongoose.Schema({
-  courseContentId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  topicId: { type: mongoose.Schema.Types.ObjectId, required: true },
   point: { type: String, required: true },
   explanation: { type: String, required: true },
   code: { type: String, required: true },
 });
 
 export interface TopicContent extends mongoose.Document {
-  courseContentId: mongoose.Schema.Types.ObjectId;
+  topicId: mongoose.Schema.Types.ObjectId;
   point: string;
   explanation: string;
   code: string;
